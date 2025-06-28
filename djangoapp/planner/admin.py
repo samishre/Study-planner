f3f5c51ba0f962bcdde7c527cdc8f1faf7b25ed7
Sami Shrestha <sami.shrestha@apexcollege.edu.np>
2025-06-28 13:06:42 +0545

    Initial commit

--- djangoapp/planner/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Subject, Topic, UserAvailability, StudySchedule
-
-admin.site.register(Subject)
-admin.site.register(Topic)
-admin.site.register(UserAvailability)
-admin.site.register(StudySchedule)
